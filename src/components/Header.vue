4f896b4c2193fa6da17f2d6e14c2617bb936cb47
Tarlor <2031121383@qq.com>
2025-08-07 16:17:10 +0800

    e

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,15 @@
 <template>
     <header>
+        <div class = 'logo one'>
+            <img src="../assets/icons/ze-star 1.svg" />
+        </div>
         <h2>专利管理系统</h2>
         <div class = 'people'>
             <h4>吱吱</h4>
             <h4>业务录入人员</h4>
+        </div>
+        <div class = 'logo two'>
+            <img src="../assets/icons/fa-user.svg" />
         </div>
     </header>
 </template>
@@ -31,9 +37,30 @@ export default {
         font-size: 28px;
         float: left;
     }
+    .logo {
+        height: 68px;
+        margin:0 15px;
+        line-height: 68px;
+    }
+    .one {
+        float: left;
+    }
     .people {
         float: right;
         font-size: 12px;
-        margin: auto  ;
+        margin: auto 0;
+        display: flex;           /* 新增 */
+        flex-direction: column;  /* 新增 */
+        justify-content: center; /* 新增 */
+        height: 68px;            /* 新增，确保与 header 高度一致 */
+        margin-right: 40px;
+    }
+    .people h4 {
+        margin:0 auto;
+        font-size:10px;
+        color:rgba(159, 184, 236, 1)
+    }
+    .two {
+        float: right;
     }
 </style>
