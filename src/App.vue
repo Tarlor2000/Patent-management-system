94114ebc469a7cb6f5bda4927c93c30f7bed3ad6
Tarlor <2031121383@qq.com>
2025-08-07 14:17:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header></Header>
+</template>
+<script setup lang="ts">
+
+</script>
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h2>专利管理系统</h2>
+        <div class = 'people'>
+            <h4>吱吱</h4>
+            <h4>业务录入人员</h4>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    header {
+        width: 100%;
+        height: 68px;
+        background-color: #000000;
+        color :#ffffff
+    }
+    header h2 {
+        font-weight: normal;
+        line-height: 68px;
+        font-size: 28px;
+        float: left;
+    }
+    .people {
+        float: right;
+        font-size: 12px;
+        margin: auto  ;
+    }
+</style>
