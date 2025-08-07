8e335ea6793d45e7ec5650af834899ea5159e405
Tarlor <2031121383@qq.com>
2025-08-07 17:50:34 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Header></Header>
+  <Sidebar></Sidebar> 
 </template>
 <script setup lang="ts">
 
 </script>
 <script>
 import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'  
 export default {
   components: {
-    Header
+    Header,
+    Sidebar
   }
 }
 
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class = 'nav'>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Sidebar"
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    .nav {
+        width: 269px;
+        height: 100vh;
+        background-color: #000000;
+        position: fixed;
+        left: 0;
+        top: 68px;
+    }
+</style>
